68e38f95cfaef99ee990a8a01a15c6170a15953b
Benoit Manhes <benoit.manhes.82@gmail.com>
2018-09-26 10:52:28 -0400

    ExceptionTir
    Ajout de l'ExceptionTir

---
+++ src/Exception/ExceptionTir.java
@@ -0,0 +1,13 @@
+package Exception;
+
+public class ExceptionTir extends Exception{
+
+	public ExceptionTir() {
+		super();
+	}
+	
+	public ExceptionTir(String message) {
+		super(message);
+	}
+
+}
